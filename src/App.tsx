21b54278f52b5940375a2982a049999c52a27b89
MAHMETT <muhamatjayakusuma2008@gmail.com>
2025-03-06 17:19:37 +0700

    add icon and image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 // import Main from './components/UI/Background';
 
+import Navbar from './components/Layout/Navbar';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
